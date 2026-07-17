e46400dda4ab63adb82bba942abb0bf1c79dbe74
jmpfbmx <jmpf.bmx@gmail.com>
2026-07-17 16:51:25 +0200

    merlinx: Link libutinterface_custom_md against libutinterface_md
    Adapted from commit 7f003994b0505a9eb1d43d8e9f6bbe47a23825c4 for the legacy shell extractor.
    
    Signed-off-by: jmpfbmx <jmpf.bmx@gmail.com>

--- extract-files.sh
+++ extract-files.sh
@@ -6,6 +6,24 @@
 # SPDX-License-Identifier: Apache-2.0
 #
 
+function blob_fixup() {
+    case "${1}" in
+        vendor/lib*/libutinterface_custom_md.so)
+            [ "$2" = "" ] && return 0
+            "$PATCHELF" --add-needed "libutinterface_md.so" "$2"
+            ;;
+        *)
+            return 1
+            ;;
+    esac
+
+    return 0
+}
+
+function blob_fixup_dry() {
+    blob_fixup "$1" ""
+}
+
 # If we're being sourced by the common script that we called,
 # stop right here. No need to go down the rabbit hole.
 if [ "${BASH_SOURCE[0]}" != "${0}" ]; then
@@ -13,6 +31,7 @@ if [ "${BASH_SOURCE[0]}" != "${0}" ]; then
 fi
 
 set -e
+
 
 export DEVICE=merlinx
 export DEVICE_COMMON=mt6768-common
